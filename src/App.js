752f083ef661755fa92ac969afb0e70d4aa4eac6
Peace Oyewole <oyewolepeace94@gmail.com>
2023-12-25 03:21:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import illustration from './illustration.svg'
+import avatar from './avatar.webp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={illustration} alt="Illustration article" className='illustration'/>
+      </div>
+      <div>
+        <button className='button-el'>Learning</button>
+        <p className='para'>Published 21 Dec 2023</p>
+        <h1>HTML & CSS foundations</h1>
+        <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+      </div>
+      <div className='footer'>
+        <img src={avatar} alt="avatar" className='avatar' />
+        <small>Greg Hooper</small>
+      </div>
     </div>
   );
 }
